adde0b8869a1ae71a0be99950a17b52198db286e
mgrupe125 <64987494+mgrupe125@users.noreply.github.com>
2020-05-07 22:53:51 -0400

    Update Form1.cs

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -22,21 +22,6 @@ namespace Client
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
-        {
-            
-        }
-
-        private void txtBrowserUrl_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             brpWebBrowser.GoBack();
